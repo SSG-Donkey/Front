af3533e4899297e1446dd07768c9ebb231c2b41c
giunchoe <z961223@gmail.com>
2024-04-22 11:21:10 +0900

    init

---
+++ front/src/main/java/com/example/front/controller/ProxyController.java
@@ -0,0 +1,25 @@
+package com.example.front.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/proxy")
+public class ProxyController {
+
+    private final RestTemplate restTemplate;
+
+    public ProxyController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/getRecentPost")
+    public ResponseEntity<String> getRecentPost() {
+        String url = "http://board.default.svc.cluster.local:8080/api_post/getRecentPost";
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        return ResponseEntity.ok(response.getBody());
+    }
+}
