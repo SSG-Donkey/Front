29663c90d307c003d32c92dbaf533c5f2defed30
Donghoon Seo <tjehdgns0925@naver.com>
2024-04-26 11:12:48 +0900

    오류수정

--- front/src/main/java/com/example/front/controller/ProxyController.java
+++ front/src/main/java/com/example/front/controller/ProxyController.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.Map;
 
 
