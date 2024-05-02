37779f6b925ba2aa8df9c4848f75271624a3e9c2
K-Gombo <rmsqh4617@gmail.com>
2024-05-02 17:55:12 +0900

    token Issue

--- front/src/main/resources/static/js/common.js
+++ front/src/main/resources/static/js/common.js
@@ -2,8 +2,10 @@
 function updateLoginStatus() {
     if (localStorage.getItem('authToken')) {
         $('#login-status').html('<a href="#" onclick="logout()">로그아웃</a>');
+        $('#mypage-link').show();  // 로그인 상태에서 마이페이지 링크 보이기
     } else {
         $('#login-status').html('<a href="loginForm.html">로그인/회원가입</a>');
+        $('#mypage-link').hide();  // 로그아웃 상태에서 마이페이지 링크 숨기기
     }
 }
 
